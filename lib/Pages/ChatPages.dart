bef610310b1f9fe827dd465c652cc559bdf94652
Dimitri PERREAUX <dimitri.perreaux@gmail.com>
2024-07-06 18:01:13 +0200

    feat(CustomCard): build dynamically from ChatModel

---
+++ lib/Pages/ChatPages.dart
@@ -0,0 +1,65 @@
+import 'package:chatapp/CustomUI/CustomCard.dart';
+import 'package:flutter/material.dart';
+import 'package:chatapp/Model/ChatModel.dart';
+
+    class ChatPage extends StatefulWidget {
+      const ChatPage({super.key});
+    
+      @override
+      State<ChatPage> createState() => _ChatPageState();
+    }
+    
+    class _ChatPageState extends State<ChatPage> {
+
+      List<ChatModel> chats = [
+        ChatModel(
+          name: "Dev Stack",
+          isGroup: false,
+          currentMessage: "Hi everyone",
+          time: "4:00",
+          icon: "person.svg"
+        ),
+        ChatModel(
+            name: "Kishor",
+            isGroup: false,
+            currentMessage: "Hi kiki",
+            time: "10:00",
+            icon: "person.svg"
+        ),
+        ChatModel(
+            name: "Collins",
+            isGroup: false,
+            currentMessage: "Hi dev Stack",
+            time: "10:00",
+            icon: "person.svg"
+        ),
+        ChatModel(
+            name: "Dev Server Chat",
+            isGroup: true,
+            currentMessage: "Hi everyone",
+            time: "10:00",
+            icon: "groups.svg"
+        ),
+        ChatModel(
+            name: "Balram Friends Stack",
+            isGroup: true,
+            currentMessage: "Hi everyone",
+            time: "8:00",
+            icon: "groups.svg"
+        ),
+      ];
+      @override
+      Widget build(BuildContext context) {
+        return Scaffold(
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            child: Icon(Icons.chat),
+          ),
+          body:ListView.builder(
+              itemCount: chats.length,
+              itemBuilder: (context, index) => CustomCard(
+                  chatModel: chats[index],))
+        );
+      }
+    }
+    
---
+++ lib/Screens/Homescreen.dart
@@ -0,0 +1,71 @@
+import 'package:chatapp/Pages/ChatPages.dart';
+import 'package:flutter/material.dart';
+
+class Homescreen extends StatefulWidget {
+  const Homescreen({super.key});
+
+  @override
+  State<Homescreen> createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen> with SingleTickerProviderStateMixin {
+  late TabController _controller;
+  @override
+  void initState() {
+    super.initState();
+    _controller = TabController(length: 4, vsync: this, initialIndex: 1);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: Text("Whatsapp Clone"),
+            actions: [
+              IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+              PopupMenuButton<String>(
+                onSelected: (value) {
+                  print(value);
+                },
+                itemBuilder: (BuildContext context) {
+                return [
+                  PopupMenuItem(child: Text("New group"), value: "New Group", ),
+                  PopupMenuItem(child: Text("New broadcast"), value: "New broadcast", ),
+                  PopupMenuItem(child: Text("What's app Web"), value: "What's app Web", ),
+                  PopupMenuItem(child: Text("Starred messages"), value: "Starred messages", ),
+                  PopupMenuItem(child: Text("Settings"), value: "Setting", ),
+                ];
+              },
+              )
+            ],
+            bottom: TabBar(
+                controller: _controller,
+                indicatorColor: Colors.white,
+              tabs: [
+                Tab(
+                  icon: Icon(Icons.camera_alt),
+                ),
+                Tab(
+                  text: "CHATS",
+                ),
+                Tab(
+                  text: "STATUS"
+                ),
+                Tab(
+                  text: "CALLS"
+                )
+              ],
+            ),
+          ),
+      body: TabBarView(
+        controller: _controller,
+        children: [
+          Text("camera"),
+          ChatPage(),
+          Text("status"),
+          Text("calls")
+        ],
+
+      ),
+        );
+  }
+}
